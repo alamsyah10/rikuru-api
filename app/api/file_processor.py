d4d15fa9c95eebce2a4a19fb1752e97e88cef29d
Hafid Sulaiman <hafidsulaiman31@gmail.com>
2025-01-27 11:39:14 +0900

    update file_processor

--- app/api/file_processor.py
+++ app/api/file_processor.py
@@ -6,26 +6,24 @@ router = APIRouter()
 @router.post("/candidate-file-processor", response_model=FileProcessingResponse)
 async def candidate_file_processor(fileProcessingRequest : FileProcessingReqquest):
 
-    # Load the Excel file
-    file_path = "data.xlsx"  # Replace with your file path
-    sheet_name = "Sheet1"  # Replace with your sheet name
-    data = pd.read_excel(file_path, sheet_name=sheet_name)
+    import pandas as pd
 
-    # Display the first few rows of the data (optional)
-    print("Original Data:")
-    print(data.head())
+    # Load the file
+    inputData = FileProcessingReqquest
+    data = pd.read_json(inputData)
 
-    # Apply a filter (e.g., selecting rows where "Age" is greater than 30)
-    filtered_data = data[data["Age"] > 30]
+    # Display the original data
+    print("Original Data: ", data.head())
 
-    # Display the filtered data (optional)
-    print("\nFiltered Data:")
-    print(filtered_data)
+    # Sort Data
+    sortData = data.sort_values(by="能力試験JLPT", ascending=True)
 
-    # Save the filtered data to a new Excel file
-    output_file_path = "filtered_data.xlsx"
-    filtered_data.to_excel(output_file_path, index=False)
+    # Remove duplicate data
+    delDupe = sortData.drop_duplicates()
+    print(delDupe)
 
-    print(f"\nFiltered data saved to {output_file_path}")
+    #Convert rows to a list
+    convDict = delDupe.to_dict(orient="list")
+    print(convDict)
 
     return 0
