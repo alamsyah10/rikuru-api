dd47447e95a11ef9d980d73dbc536a09b5ffd53d
alamsyah10 <alamsyah_imanudin0209@gmail.com>
2025-03-20 15:29:27 +0900

    fix cors

--- app/main.py
+++ app/main.py
@@ -2,23 +2,23 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from app.api import file_processor
 from app.services import chatgpt_service
+from dotenv import load_dotenv
+import os
 
+load_dotenv()
+
+ALLOWED_ORIGINS = os.getenv("ALLOWED_ORIGINS", "").split(",")
 app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],  # Allow only localhost:3000
+    allow_origins=ALLOWED_ORIGINS,  # Allow only localhost:3000
     allow_credentials=True,
     allow_methods=["*"],  # Allows all HTTP methods (GET, POST, PUT, DELETE, etc.)
     allow_headers=["*"],  # Allows all headers
 )
 
-# Include your file processor router
 app.include_router(file_processor.router)
-
-# Register routes
-# app.include_router(items.router, prefix="/items", tags=["items"])
-# app.include_router(users.router, prefix="/users", tags=["users"])
 
 @app.get("/")
 def read_root():
